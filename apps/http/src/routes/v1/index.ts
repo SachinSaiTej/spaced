bbd11a9b5e3199a71e4d7636878c40e353029dcd
SachinSaiTej <saitejakm2001@gmail.com>
2024-11-09 22:15:17 +0530

    Done with all the http endPoints.

--- apps/http/src/routes/v1/index.ts
+++ apps/http/src/routes/v1/index.ts
@@ -78,11 +78,28 @@ router.post("/signin", async (req, res) => {
     }
 })
 
-router.get("/elements", (req, res) => {
+router.get("/elements", async (req, res) => {
+    const elements = await client.element.findMany()
 
+    res.json({
+        elements: elements.map(e => ({
+            id: e.id,
+            imageUrl: e.imageUrl,
+            width: e.width,
+            height: e.height,
+            static: e.static
+        }))
+    })
 })
-router.get("/avatars", (req, res) => {
-
+router.get("/avatars", async (req, res) => {
+    const avatars = await client.avatar.findMany()
+    res.json({
+        avatars: avatars.map(a => ({
+            id: a.id,
+            imageUrl: a.imageUrl,
+            name: a.name
+        }))
+    })
 })
 
 router.use("/user", userRouter);
